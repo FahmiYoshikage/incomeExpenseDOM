4f1eb0cc321e908bc0a941a31dfae562ce52a219
FahmiVoldigoad <fahmiilham029@gmail.com>
2025-01-03 06:39:11 +0700

    javascript main function done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
+const fs = require('fs');
+
+// DOM getElementById
 let incomeButton = document.getElementById('incomeButton');
 let expenseButton = document.getElementById('expenseButton');
+let totalBalanceElement = document.getElementById('totalBalance');
 
+// Global Variable
+let Transaction = {
+    income: [],
+    expense: []
+}
+
+let totalBalance = 0;
+
+// Function
+function updateTotalBalance() {
+    totalBalanceElement.innerHTML = `Rp. ${totalBalance},00`;
+}
+
+// DOM addEventListener
 incomeButton.addEventListener('click', () => {
     let inputTransaction = document.getElementById('inputTransaction').value;
     let inputAmount = document.getElementById('inputAmount').value;
     let incomeTransaction = document.getElementById('incomeTransaction');
 
-    incomeTransaction.innerHTML += `<li>${inputTransaction} <span>${inputAmount}</span></li>`;
+    incomeTransaction.innerHTML = '';
+
+    let tempObj = {
+        transaction: inputTransaction,
+        amount: +inputAmount
+    }
+
+    Transaction.income.push(tempObj);
+
+    Transaction.income.forEach(item => {
+        incomeTransaction.innerHTML += `<li>${item.transaction} 
+        <span>Rp. ${item.amount},00</span>
+        </li>`;
+    })
+    totalBalance += +inputAmount;
+    updateTotalBalance();
 });
 
 expenseButton.addEventListener('click', () => {
@@ -14,7 +47,23 @@ expenseButton.addEventListener('click', () => {
     let inputAmount = document.getElementById('inputAmount').value;
     let expenseTransaction = document.getElementById('expenseTransaction');
 
-    expenseTransaction.innerHTML += `<li>${inputTransaction} <span>${inputAmount}</span></li>`;
+    expenseTransaction.innerHTML = '';
+
+    let tempObj = {
+        transaction: inputTransaction,
+        amount: +inputAmount
+    }
+
+    Transaction.expense.push(tempObj);
+
+    Transaction.expense.forEach(item => {
+        expenseTransaction.innerHTML += `<li>${item.transaction} 
+        <span>Rp. ${item.amount},00</span>
+        </li>`;
+    })
+    totalBalance -= +inputAmount;
+    updateTotalBalance();
 })
 
-console.log(incomeButton, expenseButton);
+// Call Function
+updateTotalBalance();
